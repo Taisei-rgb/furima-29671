624cc19811e7a1b0365554b6c527534e6688c98d
Taisei-rgb <taisei.contact@gmail.com>
2020-09-10 21:08:30 +0900

    単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,24 +7,23 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   # email情報はdeviseにデフォルトで入っているため不要
-  
+
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # validates :email, presence: true, uniqueness: true,
   # format: { with: VALID_EMAIL_REGEX }
 
 
-  VALID_PASSWORD_REGEX = /^[a-z0-9]+$/
-  validates :password, presence: true, length: { minimum: 6 },
-  confirmation: true
-  format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  # validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates_format_of :password, with: VALID_PASSWORD_REGEX
 
   validates :birth_date, presence: true
 
-  ZENKAKU_INPUT = /^[ぁ-んァ-ン一-龥]/
+  ZENKAKU_INPUT = /\A[ぁ-んァ-ン一-龥]+\z/
   validates :family_name, :first_name, presence: true,
   format: { with: ZENKAKU_INPUT }
 
-  ZENKAKU_KANA_INPUT = /^([ァ-ン]|ー)+$/
+  ZENKAKU_KANA_INPUT = /\A([ァ-ン]|ー)+\z/
   validates :family_name_kana, :first_name_kana, presence: true,
   format: { with: ZENKAKU_KANA_INPUT }
 
