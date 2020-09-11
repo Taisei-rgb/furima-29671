7100c98b1b3241a3e326fc783da125f56e989813
Taisei-rgb <taisei.contact@gmail.com>
2020-09-11 11:06:08 +0900

    ユーザー管理機能の実装（修正２回目）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 
+          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+          validates_format_of :password, with: VALID_PASSWORD_REGEX
 
-       with options presence: true do
-              validates :nickname, 
-              VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-              validates_format_of :password, with: VALID_PASSWORD_REGEX
+          ZENKAKU_INPUT = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+          validates :family_name, :first_name, format: { with: ZENKAKU_INPUT }
 
-              validates :birth_date, 
+          ZENKAKU_KANA_INPUT = /\A([ァ-ン]|ー)+\z/.freeze
+          validates :family_name_kana, :first_name_kana, format: { with: ZENKAKU_KANA_INPUT }
 
-              ZENKAKU_INPUT = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-              validates :family_name, :first_name,
-              format: { with: ZENKAKU_INPUT }
-
-              ZENKAKU_KANA_INPUT = /\A([ァ-ン]|ー)+\z/.freeze
-              validates :family_name_kana, :first_name_kana,
-              format: { with: ZENKAKU_KANA_INPUT }
+       with_options presence: true do
+              validates :nickname
+              validates :birth_date
        end
 end
