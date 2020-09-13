de3e3dd7d1afbee64e4e6ff17df71f0c6da2651c
Taisei-rgb <taisei.contact@gmail.com>
2020-09-13 14:08:34 +0900

    出品情報をDBに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.save!
       redirect_to root_path
     else
       render action: :new
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:category_id, :prefecture_id, :shipping_fee_id, :status_id, :waiting_date_id,:name, :description, :price, :image)
+    params.require(:item).permit(:category_id, :prefecture_id, :shipping_fee_id, :status_id, :waiting_date_id,:name, :description, :price, :image).merge(user_id: current_user.id)
   end
 end
