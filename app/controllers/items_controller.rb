071c0065cf36def7e048f53a91b61fd2bec0a0be
Taisei-rgb <taisei.contact@gmail.com>
2020-09-14 17:33:20 +0900

    商品一覧表示機能の実装（修正１回目）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :unless_login_user, only: :new
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
