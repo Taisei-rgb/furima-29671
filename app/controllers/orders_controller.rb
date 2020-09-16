bd1400280ad2635ad2524efb7e44582a0a8e4d2f
Taisei-rgb <taisei.contact@gmail.com>
2020-09-16 15:53:12 +0900

    ページ遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_seller, only: :index
+  before_action :redirect_seller, only: :index
 
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
+  end
+
+  def find_seller
+    @item = Item.find(params[:item_id])
+  end
+  
+  def redirect_seller
+    if @item.user_id ==  current_user.id
+      redirect_to root_path
+    end
   end
 end
